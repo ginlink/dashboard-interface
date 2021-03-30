f0762c249aaa01b2497e92cdb51bff5125f44ff8
liuyi <liudafengshu@gmail.com>
2021-03-30 15:43:10 +0800

    修改了一键earn和一键harvest的运行bug

--- src/store/investment/modules.js
+++ src/store/investment/modules.js
@@ -1,26 +1,32 @@
 import { Map } from "immutable";
-import * as types from "./actionTypes"
+import * as types from "./actionTypes";
 
-const reducer = ( state = Map({
-    web3:{},
-    account:"",
-    dataArray:[],
-    addresses:[],
-    owners:[]
-}) , action) =>{
-    switch(action.type){
-        case  types.CHANGEWEB3 :
-            return state.set("web3",action.payload)
-        case types.CHANGEACCOUNT:
-            return state.set("account",action.payload)
-        case types.CHANGEDATARRAY:
-            return state.set("dataArray",action.payload)
-        case types.CHANGEADDRESSES:
-            return state.set("addresses",action.payload)
-        case types.CHANGEOWNERS:
-            return state.set("owners",action.payload)
-        default:
-            return state
-    }
-}
-export default reducer
+const reducer = (
+  state = Map({
+    web3: {},
+    account: "",
+    dataArray: [],
+    addresses: [],
+    owners: [],
+    investmentListOnLoad: false,
+  }),
+  action
+) => {
+  switch (action.type) {
+    case types.CHANGEWEB3:
+      return state.set("web3", action.payload);
+    case types.CHANGEACCOUNT:
+      return state.set("account", action.payload);
+    case types.CHANGEDATARRAY:
+      return state.set("dataArray", action.payload);
+    case types.CHANGEADDRESSES:
+      return state.set("addresses", action.payload);
+    case types.CHANGEOWNERS:
+      return state.set("owners", action.payload);
+    case types.CHAGNEINVESTMENTLISTONLOAD:
+      return state.set("investmentListOnLoad", true);
+    default:
+      return state;
+  }
+};
+export default reducer;
