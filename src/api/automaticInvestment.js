15ee1b70b5ceeae029837ad977c7d5b2d8251c23
zhangyang <1105626801@qq.com>
2021-10-28 14:32:03 +0800

    init

--- src/api/automaticInvestment.js
+++
@@ -1,8 +0,0 @@
-//自动化投资页面api
-import request from "./../utils/request"
-
-// 获取自动化投资列表
-export function getAutomaticInvestmentList (params) {
-    return request.get("/getTaskList" , params)
-}
-
--- src/utils/request.js
+++
@@ -1,9 +0,0 @@
-//封装请求
-
-import axios from "axios"
-
-const request = axios.create({
-    baseURL:"http://192.168.3.80:9800"
-})
-
-export default request
