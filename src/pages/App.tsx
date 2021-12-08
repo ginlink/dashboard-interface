27eca66ff661727e673fbb952f7436e2715b25ca
蒋金 <ginlink@163.com>
2021-12-08 11:14:11 +0800

    ref: 调整侧边栏路由逻辑

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import { Redirect, Route, Switch } from 'react-router'
 import Home from './Home'
-// import Contract from './Contract'
+import Contract from './Contract'
 
 export default function App() {
   return (
     <>
       <Switch>
         <Route strict exact path="/home" component={Home} />
-        {/* <Route strict exact path="/contract" component={Contract} /> */}
+        <Route strict exact path="/contract" component={Contract} />
 
         <Redirect from="/" to="/home" />
       </Switch>
