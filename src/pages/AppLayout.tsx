3dd214d22f82ecfe9a08924cfdbbb2b9f1ce4307
蒋金 <ginlink@163.com>
2021-12-08 11:03:35 +0800

    style: 清洁

--- src/pages/AppLayout.tsx
+++ src/pages/AppLayout.tsx
@@ -1,19 +1,13 @@
-import { Layout, Menu, Breadcrumb } from 'antd'
-import { DesktopOutlined, PieChartOutlined, FileOutlined, TeamOutlined, UserOutlined } from '@ant-design/icons'
 import React from 'react'
+import { Layout } from 'antd'
 import styled from 'styled-components'
 
 import PCHeader from '../components/Header'
 import Sider from '../components/Sider'
-const { Header, Content } = Layout
-const { SubMenu } = Menu
-const SiderLogo = styled.div`
-  height: 32px;
-  margin: 16px;
-  background: rgba(255, 255, 255, 0.3);
-`
 
-const HaderBar = styled.div`
+const { Content } = Layout
+
+const HeaderBar = styled.div`
   height: 60px;
   background: #fff;
 `
@@ -28,9 +22,9 @@ export default function AppLayout({ children }: { children: any }) {
     <Layout style={{ minHeight: '100vh' }}>
       <Sider></Sider>
       <Layout className="site-layout">
-        <HaderBar>
+        <HeaderBar>
           <PCHeader />
-        </HaderBar>
+        </HeaderBar>
         <Content style={{ margin: '16px 16px' }}>
           <LayoutContent>{children}</LayoutContent>
         </Content>
