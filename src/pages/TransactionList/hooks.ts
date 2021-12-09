cb2108d1953c0bf63fa739680a4999989560125d
蒋金 <ginlink@163.com>
2021-12-09 18:56:28 +0800

    feat: tmp add

--- src/pages/TransactionList/hooks.ts
+++ src/pages/TransactionList/hooks.ts
@@ -10,35 +10,43 @@ import {
 import { buildMultiSendSafeTx } from '@/utils/multisend'
 import { useTransactionMultiSend } from '@/hooks/useContract'
 import { useMemo } from 'react'
+import { Erc20 } from '@/abis/types'
 
-export function useTransacitonSubmitData(
-  contract: any | null,
-  method: string,
-  params: any[],
-  nonce: number,
-  chainId: string,
-  safe: string
-) {
+export type TransactionSubmitProps = {
+  contract?: Erc20 | null
+  safe?: string
+  method?: string
+  params?: [string, BigNumberish] | undefined
+  nonce?: number
+  chainId?: number | undefined
+}
+
+export function useTransacitonSubmitData({ contract, safe, method, params, nonce, chainId }: TransactionSubmitProps) {
   const multiSend = useTransactionMultiSend()
+
   // const txs = useMemo(() => {
   //   if (!contract || !method || !params) return null
   //   return [buildContractCall(contract, method, params, 0)]
   // }, [contract, method, params])
+
   const txs = useMemo(() => {
-    if (!contract || !method || !params) return null
+    if (!contract || !method || !params || !nonce) return null
     const data = contract.interface.encodeFunctionData('transfer', params)
-    console.log(data)
-    console.log('contract.address', contract.address)
+
+    // console.log(data)
+    // console.log('contract.address', contract.address)
     return [buildSafeTransaction({ to: contract.address, data, safeTxGas: 1000000, nonce: nonce })]
   }, [contract, method, nonce, params])
+
   const safeTx = useMemo(() => {
-    if (!multiSend || !txs) return null
+    if (!multiSend || !txs || !nonce) return null
     return buildMultiSendSafeTx(multiSend, txs, nonce)
   }, [multiSend, nonce, txs])
 
   const safeApproveHash = useMemo(() => {
-    if (!safeTx) return null
-    return calculateSafeTransactionHash(safe, safeTx, chainId)
+    if (!safeTx || !chainId || !safe) return null
+
+    return calculateSafeTransactionHash(safe, safeTx, chainId + '')
   }, [chainId, safe, safeTx])
 
   return {
