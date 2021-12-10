a769d013af1a057b7aae9638e65735248b372637
蒋金 <ginlink@163.com>
2021-12-10 21:47:39 +0800

    style: 修改api地址为15服务器

--- src/services/config.ts
+++ src/services/config.ts
@@ -9,7 +9,8 @@ export const BSC_URL = isDev ? 'testnet.bscscan.com' : 'bscscan.com'
 // export const BASE_URL = isDev ? 'http://16.162.188.15:9010/api' : 'http://16.162.188.15:9010/api'
 
 //TODO 测试，记得改回来
-export const BASE_URL = 'http://192.168.3.45:9771'
+// export const BASE_URL = 'http://192.168.3.45:9771'
+export const BASE_URL = 'http://16.162.188.15:9701'
 // export const BASE_URL = isDev ? 'http://localhost:9991/api' : 'http://16.162.188.15:9010/api'
 
 export const BASE_URL_15 = 'http://16.162.188.15:9010/api'
