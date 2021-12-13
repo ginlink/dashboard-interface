8b586f5cafe28c8760a6c127dc3d1325d2596622
zhangyang <1105626801@qq.com>
2021-12-13 10:41:56 +0800

    owner

--- src/constants/addresses.ts
+++ src/constants/addresses.ts
@@ -23,7 +23,7 @@ export const TRANSACTION_PROXY_ADDRESS: AddressMap = {
 }
 export const TRANSACTION_OPERATABLE_ADDRESS: AddressMap = {
   // [SupportedChainId.BSCTEST]: '0x15487E4f8f2064F29E559ec93b136a666f4CaB45',
-  [SupportedChainId.BSCTEST]: '0x29D2bEd6E240e015E09E0d2eDEFA8A55A8799671',
+  [SupportedChainId.BSCTEST]: '0x15487E4f8f2064F29E559ec93b136a666f4CaB45',
   [SupportedChainId.BSC]: '',
 }
 
